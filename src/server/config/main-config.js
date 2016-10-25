8d11f71920f6a371b3c7c13f0b627d919327b7f4
Brandon Blair <brandon.d.blair@gmail.com>
2016-10-24 20:12:11 -0600

    Updated Server

--- src/server/config/main-config.js
+++ src/server/config/main-config.js
@@ -25,14 +25,12 @@
     // *** cross domain requests *** //
     const allowCrossDomain = function(req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
-      // res.header('Access-Control-Allow-Methods', 'GET ,PUT, POST, DELETE');
-      // res.header('Access-Control-Allow-Headers', 'Content-Type');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
       next();
     };
 
-
     app.use(allowCrossDomain);
-
     app.use(cookieParser());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
